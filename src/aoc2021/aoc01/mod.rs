4758f09c5a0ebe228c185813fcec749973f25250
Jona Löffler <jona.loeffler@online.de>
2022-07-18 15:44:51 +0200

    Refactor 2021-01

--- src/aoc2021/aoc01/mod.rs
+++ src/aoc2021/aoc01/mod.rs
@@ -10,48 +10,33 @@ fn read() -> Vec<i32> {
         .collect()
 }
 
-fn count((i, n): (usize, &i32), numbers: &Vec<i32>) -> Option<bool> {
-    if numbers.get(i - 1)? < n {
-        Some(true)
-    } else {
-        None
-    }
-}
-
-fn windows((i, n): (usize, &i32), numbers: &Vec<i32>) -> Option<i32> {
-    let first = numbers.get(i - 1)?;
-    let second = numbers.get(i - 2)?;
-
-    Some(n + first + second)
-}
-
 pub fn one() -> usize {
-    let numbers = read();
-
-    numbers
-        .iter()
-        .enumerate()
-        .skip(1)
-        .filter_map(|t| count(t, &numbers))
+    read()
+        .windows(2)
+        .filter_map(|window| {
+            if window.get(0)? < window.get(1)? {
+                Some(true)
+            } else {
+                None
+            }
+        })
         .collect::<Vec<bool>>()
         .len()
 }
 
 pub fn two() -> usize {
-    let numbers = read();
-
-    let windowed: Vec<i32> = numbers
-        .iter()
-        .enumerate()
-        .skip(2)
-        .filter_map(|t| windows(t, &numbers))
-        .collect();
-
-    windowed
-        .iter()
-        .enumerate()
-        .skip(1)
-        .filter_map(|t| count(t, &windowed))
+    read()
+        .windows(3)
+        .map(|window| window.iter().sum())
+        .collect::<Vec<i32>>()
+        .windows(2)
+        .filter_map(|window| {
+            if window.get(0)? < window.get(1)? {
+                Some(true)
+            } else {
+                None
+            }
+        })
         .collect::<Vec<bool>>()
         .len()
 }
