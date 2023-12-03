0de63a7e0636a9b34a52a3d5cb0b5702d4046b07
Jona Löffler <>
2023-12-03 16:02:37 +0100

    Update random stuff

--- src/aoc2022/aoc07/mod.rs
+++ src/aoc2022/aoc07/mod.rs
@@ -1,149 +1,150 @@
-// use std::collections::{hash_map::Keys, HashMap};
+use std::collections::{hash_map::Keys, HashMap};
 
-// static INPUT: &'static str = include_str!("./input");
+static INPUT: &'static str = include_str!("./input");
 
-// #[derive(Clone, Debug)]
-// struct Tree {
-//     dirs: HashMap<String, Box<Tree>>,
-//     files: HashMap<String, usize>,
-// }
+#[derive(Clone, Debug)]
+struct Tree {
+    dirs: HashMap<String, Box<Tree>>,
+    files: HashMap<String, usize>,
+}
 
-// impl Tree {
-//     fn new() -> Self {
-//         Self {
-//             dirs: HashMap::new(),
-//             files: HashMap::new(),
-//         }
-//     }
+impl Tree {
+    fn new() -> Self {
+        Self {
+            dirs: HashMap::new(),
+            files: HashMap::new(),
+        }
+    }
 
-//     fn iter(&self) -> TreeIterator {
-//         TreeIterator {
-//             tree: self.clone(),
-//             path: self.dirs.clone().keys().cloned(),
-//             has_returned_self: false,
-//         }
-//     }
+    // fn iter(&self) -> TreeIterator {
+    //     TreeIterator {
+    //         tree: self.clone(),
+    //         path: self.dirs.clone().keys().cloned(),
+    //         has_returned_self: false,
+    //     }
+    // }
 
-//     fn size(&self) -> usize {
-//         self.files.iter().map(|(_, v)| v).sum::<usize>()
-//             + self.dirs.iter().map(|(_, t)| t.size()).sum::<usize>()
-//     }
-// }
+    fn size(&self) -> usize {
+        self.files.iter().map(|(_, v)| v).sum::<usize>()
+            + self.dirs.iter().map(|(_, t)| t.size()).sum::<usize>()
+    }
+}
 
-// // impl IntoIterator for Tree {
-// //     type Item = Tree;
-
-// //     type IntoIter = TreeIterator<'_>
-
-// //     fn into_iter(self) -> Self::IntoIter {
-// //         self.iter()
-// //     }
-// // }
-
-// struct TreeIterator<'a> {
-//     tree: Tree,
-//     path: std::iter::Cloned<Keys<'a, String, Box<Tree>>>,
-//     has_returned_self: bool,
-// }
-
-// impl Iterator for TreeIterator<'_> {
+// impl IntoIterator for Tree {
 //     type Item = Tree;
 
-//     fn next(&mut self) -> Option<Self::Item> {
-//         if let Some(path) = self.path.next() {
-//             return self.tree.dirs.get(&path).map(|t| *t.to_owned());
-//         } else if !self.has_returned_self {
-//             self.has_returned_self = true;
-//             return Some(self.tree.clone());
-//         }
+//     type IntoIter = TreeIterator<'_>
 
-//         None
+//     fn into_iter(self) -> Self::IntoIter {
+//         self.iter()
 //     }
 // }
 
-// impl Default for Tree {
-//     fn default() -> Self {
-//         Tree::new()
-//     }
-// }
+struct TreeIterator<'a> {
+    tree: Tree,
+    path: std::iter::Cloned<Keys<'a, String, Box<Tree>>>,
+    has_returned_self: bool,
+}
 
-// fn parse() -> Tree {
-//     let mut tree = Tree::default();
-//     let mut cwd: Vec<String> = vec![];
+impl Iterator for TreeIterator<'_> {
+    type Item = Tree;
 
-//     for line in INPUT.lines() {
-//         let mut split = line.split(" ");
+    fn next(&mut self) -> Option<Self::Item> {
+        if let Some(path) = self.path.next() {
+            return self.tree.dirs.get(&path).map(|t| *t.to_owned());
+        } else if !self.has_returned_self {
+            self.has_returned_self = true;
+            return Some(self.tree.clone());
+        }
 
-//         let (fst, snd, thd) = (split.next().unwrap(), split.next(), split.next());
+        None
+    }
+}
 
-//         match fst {
-//             "$" => match snd {
-//                 Some("ls") => continue,
-//                 Some("cd") => match thd {
-//                     Some("/") => cwd = vec![],
-//                     Some("..") => {
-//                         cwd.pop();
-//                         ()
-//                     }
-//                     Some(dir) => cwd.push(dir.to_string()),
-//                     _ => panic!("Wrong use of cd"),
-//                 },
-//                 _ => panic!("Empty command"),
-//             },
-//             "dir" => match snd {
-//                 Some(dir) => insert_dir_at(dir.to_string(), &mut tree, cwd.clone()),
-//                 _ => panic!("Empty dir"),
-//             },
-//             size => match snd {
-//                 Some(file) => insert_file_at(
-//                     file.to_string(),
-//                     size.parse().unwrap(),
-//                     &mut tree,
-//                     cwd.clone(),
-//                 ),
-//                 _ => panic!("Invalid file listing {line}"),
-//             },
-//         }
-//     }
+impl Default for Tree {
+    fn default() -> Self {
+        Tree::new()
+    }
+}
 
-//     tree
-// }
+fn parse() -> Tree {
+    let mut tree = Tree::default();
+    let mut cwd: Vec<String> = vec![];
 
-// fn insert_dir_at(name: String, tree: &mut Tree, path: Vec<String>) {
-//     let mut tree: &mut Tree = tree;
-//     // println!("insert dir {name} at {:?} in tree {:#?}", path, tree);
+    for line in INPUT.lines() {
+        let mut split = line.split(" ");
 
-//     for dir in path {
-//         tree = tree.dirs.get_mut(&dir).unwrap();
-//     }
+        let (fst, snd, thd) = (split.next().unwrap(), split.next(), split.next());
 
-//     tree.dirs.insert(name, Box::new(Tree::new()));
-// }
+        match fst {
+            "$" => match snd {
+                Some("ls") => continue,
+                Some("cd") => match thd {
+                    Some("/") => cwd = vec![],
+                    Some("..") => {
+                        cwd.pop();
+                        ()
+                    }
+                    Some(dir) => cwd.push(dir.to_string()),
+                    _ => panic!("Wrong use of cd"),
+                },
+                _ => panic!("Empty command"),
+            },
+            "dir" => match snd {
+                Some(dir) => insert_dir_at(dir.to_string(), &mut tree, cwd.clone()),
+                _ => panic!("Empty dir"),
+            },
+            size => match snd {
+                Some(file) => insert_file_at(
+                    file.to_string(),
+                    size.parse().unwrap(),
+                    &mut tree,
+                    cwd.clone(),
+                ),
+                _ => panic!("Invalid file listing {line}"),
+            },
+        }
+    }
 
-// fn insert_file_at(name: String, size: usize, tree: &mut Tree, path: Vec<String>) {
-//     let mut tree: &mut Tree = tree;
-//     // println!("insert file {name} at {:?} in tree {:#?}", path, tree);
+    tree
+}
 
-//     for dir in path {
-//         tree = tree.dirs.get_mut(&dir).unwrap();
-//     }
+fn insert_dir_at(name: String, tree: &mut Tree, path: Vec<String>) {
+    let mut tree: &mut Tree = tree;
+    // println!("insert dir {name} at {:?} in tree {:#?}", path, tree);
 
-//     tree.files.insert(name, size);
-// }
+    for dir in path {
+        tree = tree.dirs.get_mut(&dir).unwrap();
+    }
 
-// pub fn one() -> Option<String> {
-//     let root = parse();
-//     println!("final tree {:#?}", root);
+    tree.dirs.insert(name, Box::new(Tree::new()));
+}
 
-//     let result = &root
-//         .iter()
-//         .map(|t| t.size())
-//         .filter(|s| s > &100000)
-//         .sum::<usize>();
+fn insert_file_at(name: String, size: usize, tree: &mut Tree, path: Vec<String>) {
+    let mut tree: &mut Tree = tree;
+    // println!("insert file {name} at {:?} in tree {:#?}", path, tree);
 
-//     Some(result.to_string())
-// }
+    for dir in path {
+        tree = tree.dirs.get_mut(&dir).unwrap();
+    }
 
-// pub fn two() -> Option<String> {
-//     todo!()
-// }
+    tree.files.insert(name, size);
+}
+
+pub fn one() -> Option<String> {
+    let root = parse();
+    println!("final tree {:#?}", root);
+
+    let result = &root
+        .iter()
+        .map(|t| t.size())
+        .filter(|s| s > &100000)
+        .sum::<usize>();
+
+    Some(result.to_string())
+}
+
+pub fn two() -> Option<String> {
+    todo!()
+}
+
--- src/aoc2022/aoc08/mod.rs
+++ src/aoc2022/aoc08/mod.rs
@@ -1,9 +1,166 @@
 static INPUT: &'static str = include_str!("./input");
 
+fn read() -> Vec<Vec<usize>> {
+    INPUT
+        .lines()
+        .map(|l| {
+            l.chars()
+                .map(|s| s.to_string().parse::<usize>().unwrap())
+                .collect()
+        })
+        .collect()
+}
+
 pub fn one() -> Option<String> {
-    None
+    let input = read();
+
+    let mut visible: Vec<Vec<bool>> = input
+        .iter()
+        .enumerate()
+        .map(|(i, k)| {
+            k.iter()
+                .enumerate()
+                .map(|(j, _)| {
+                    i == 0
+                        || j == 0
+                        || i == input.len() - 1
+                        || j == input.first().unwrap().len() - 1
+                })
+                .collect()
+        })
+        .collect();
+
+    let rows = input.len();
+    let cols = input.first().unwrap().len();
+
+    for i in 0..rows - 1 {
+        let mut highest = 0;
+        for j in 1..cols - 1 {
+            let next = input.get(i).unwrap().get(j).unwrap();
+            let prev = input.get(i).unwrap().get(j - 1).unwrap();
+
+            if prev > &highest {
+                highest = prev.clone();
+            }
+
+            if next > &highest {
+                *visible.get_mut(i).unwrap().get_mut(j).unwrap() = true;
+            }
+        }
+        highest = 0;
+        for j in (0..cols - 1).rev() {
+            let next = input.get(i).unwrap().get(j).unwrap();
+            let prev = input.get(i).unwrap().get(j + 1).unwrap();
+
+            if prev > &highest {
+                highest = prev.clone();
+            }
+
+            if next > &highest {
+                *visible.get_mut(i).unwrap().get_mut(j).unwrap() = true;
+            }
+        }
+    }
+
+    for j in 0..cols - 1 {
+        let mut highest = 0;
+        for i in 1..rows - 1 {
+            let next = input.get(i).unwrap().get(j).unwrap();
+            let prev = input.get(i - 1).unwrap().get(j).unwrap();
+
+            if prev > &highest {
+                highest = prev.clone();
+            }
+
+            if next > &highest {
+                *visible.get_mut(i).unwrap().get_mut(j).unwrap() = true;
+            }
+        }
+        highest = 0;
+        for i in (0..rows - 1).rev() {
+            let next = input.get(i).unwrap().get(j).unwrap();
+            let prev = input.get(i + 1).unwrap().get(j).unwrap();
+
+            if prev > &highest {
+                highest = prev.clone();
+            }
+
+            if next > &highest {
+                *visible.get_mut(i).unwrap().get_mut(j).unwrap() = true;
+            }
+        }
+    }
+
+    let visible_count = visible.iter().flatten().filter(|b| **b).count();
+
+    Some(visible_count.to_string())
 }
 
 pub fn two() -> Option<String> {
-    None
+    let input = read();
+
+    let mut score: Vec<Vec<i32>> = input
+        .iter()
+        .map(|k| k.iter().map(|_| 0).collect())
+        .collect();
+
+    let rows = input.len();
+    let cols = input.first().unwrap().len();
+
+    for line in &input {
+        println!("{:?}", line);
+    }
+
+    for i in 0..rows {
+        for j in 0..cols {
+            println!("looking at point {:?}", (i, j));
+            let mut left = 0;
+            let mut right = 0;
+            let mut top = 0;
+            let mut bottom = 0;
+
+            println!("{}", "---left---");
+            let highest = input.get(i).unwrap().get(j).unwrap();
+            for k in (i + 1)..rows - 1 {
+                let next = input.get(k).unwrap().get(j).unwrap();
+
+                println!("next: {:?}", (k, j, next, highest));
+
+                if next >= highest {
+                    println!("{}", "breaking");
+                    break;
+                } else {
+                    println!("{}", "incrementing");
+                    left += 1;
+                }
+            }
+
+            println!("{}", "---right---");
+            let highest = input.get(i).unwrap().get(j).unwrap();
+            for k in (0..i).rev() {
+                let next = input.get(k).unwrap().get(j).unwrap();
+
+                println!("next: {:?}", (k, j, next, highest));
+
+                if next >= highest {
+                    println!("{}", "breaking");
+                    break;
+                } else {
+                    println!("{}", "incrementing");
+                    right += 1;
+                }
+            }
+
+            println!("scores: {:?}", (left, right, bottom, top));
+            *score.get_mut(i).unwrap().get_mut(j).unwrap() = left * right * top * bottom;
+        }
+    }
+
+    for line in &score {
+        println!("{:?}", line);
+    }
+
+    let highscore = score.iter().flatten().max().unwrap();
+
+    Some(highscore.to_string())
 }
