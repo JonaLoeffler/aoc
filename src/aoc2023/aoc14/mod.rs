28017245ef06fa7e5a35d5bb7ac888af2db2207e
Jona Löffler <jona.loeffler@online.de>
2023-12-14 16:13:44 +0100

    Solve 2023-14 + add clap

---
+++ src/aoc2023/aoc14/mod.rs
@@ -0,0 +1,101 @@
+use std::collections::HashMap;
+
+static INPUT: &'static str = include_str!("./input");
+
+fn parse() -> Vec<String> {
+    INPUT.lines().map(|l| l.to_string()).collect()
+}
+
+fn rotate(g: Vec<String>) -> Vec<String> {
+    let transposed = transpose(&g.iter().map(|l| l.chars().collect()).collect())
+        .into_iter()
+        .map(|l| l.iter().rev().collect::<String>())
+        .collect();
+
+    transposed
+}
+
+fn transpose(inner: &Vec<Vec<char>>) -> Vec<Vec<char>> {
+    (0..inner[0].len())
+        .map(|i| {
+            inner
+                .iter()
+                .map(|inn| inn[i].clone())
+                .collect::<Vec<char>>()
+        })
+        .collect()
+}
+
+fn slide_right(s: String) -> String {
+    let mut s = s;
+    while s.contains("O.") {
+        s = s.replacen("O.", ".O", 1);
+    }
+    s
+}
+
+pub fn one() -> Option<String> {
+    let res: Vec<String> = rotate(parse()).into_iter().map(slide_right).collect();
+
+    let r: usize = weight(&rotate(rotate(rotate(res))));
+
+    Some(r.to_string())
+}
+
+fn weight(grid: &Vec<String>) -> usize {
+    grid.iter()
+        .rev()
+        .enumerate()
+        .map(|(i, l)| (i + 1) * (l.chars().filter(|c| c == &'O').count()))
+        .sum()
+}
+
+pub fn two() -> Option<String> {
+    let mut grid = parse();
+    let mut initial = grid.clone();
+
+    let mut grids: HashMap<Vec<String>, usize> = HashMap::new();
+
+    let full_iterations = 1000000000;
+
+    for i in 0..full_iterations {
+        grid = grid_after_full_iteration(grid);
+
+        if let Some(o) = grids.get(&grid) {
+            let remaining = (full_iterations - i) % (i - o);
+
+            for _ in 0..(i + remaining) {
+                initial = grid_after_full_iteration(initial);
+            }
+
+            return Some(weight(&initial).to_string());
+        } else {
+            grids.insert(grid.clone(), i);
+        }
+    }
+
+    Some(weight(&grid).to_string())
+}
+
+fn grid_after_full_iteration(grid: Vec<String>) -> Vec<String> {
+    let mut grid = grid;
+
+    // north
+    grid = rotate(grid).into_iter().map(slide_right).collect();
+    grid = rotate(grid);
+
+    // west
+    grid = grid.into_iter().map(slide_right).collect();
+    grid = grid;
+
+    // south
+    grid = rotate(grid).into_iter().map(slide_right).collect();
+    grid = rotate(grid);
+
+    // east
+    grid = grid.into_iter().map(slide_right).collect();
+
+    grid
+}
+
+mod tests {}
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,24 @@
 #![feature(iter_map_windows)]
+use chrono::Datelike;
+use clap::Parser;
 use std::time::{Duration, Instant};
 
 mod aoc2021;
 mod aoc2022;
 mod aoc2023;
+
+/// Simple program to greet a person
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    /// What year to execute
+    #[arg(short, long)]
+    year: Option<i32>,
+
+    /// What day to execute
+    #[arg(short, long)]
+    day: Option<u32>,
+}
 
 type DayResult = (Duration, String, String);
 
@@ -26,85 +41,98 @@ fn print(day: &str, res: DayResult) -> () {
 }
 
 fn main() {
-    println!(
-        "2021-01: {} {}",
-        aoc2021::aoc01::one(),
-        aoc2021::aoc01::two()
-    );
+    let args = Args::parse();
+    let date = chrono::Local::now();
 
-    println!(
-        "2021-02: {} {}",
-        aoc2021::aoc02::one(),
-        aoc2021::aoc02::two()
-    );
-
-    println!(
-        "2021-03: {} {}",
-        aoc2021::aoc03::one(),
-        aoc2021::aoc03::two()
-    );
-    println!(
-        "2021-04: {} {}",
-        aoc2021::aoc04::one(),
-        aoc2021::aoc04::two()
-    );
-    println!(
-        "2022-01: {} {}",
-        aoc2022::aoc01::one(),
-        aoc2022::aoc01::two()
-    );
-    println!(
-        "2022-02: {} {}",
-        aoc2022::aoc02::one(),
-        aoc2022::aoc02::two()
-    );
-    println!(
-        "2022-03: {} {}",
-        aoc2022::aoc03::one(),
-        aoc2022::aoc03::two()
-    );
-    println!(
-        "2022-04: {} {}",
-        aoc2022::aoc04::one(),
-        aoc2022::aoc04::two()
-    );
-    println!(
-        "2022-05: {} {}",
-        aoc2022::aoc05::one(),
-        aoc2022::aoc05::two()
-    );
-    println!(
-        "2022-06: {} {}",
-        aoc2022::aoc06::one().unwrap_or_default(),
-        aoc2022::aoc06::two().unwrap_or_default()
-    );
-    println!(
-        "2022-07: {} {}",
-        aoc2022::aoc07::one().unwrap_or_default(),
-        aoc2022::aoc07::two().unwrap_or_default()
-    );
-    println!(
-        "2022-08: {} {}",
-        aoc2022::aoc08::one().unwrap_or_default(),
-        aoc2022::aoc08::two().unwrap_or_default()
-    );
-    println!(
-        "2022-09: {} {}",
-        aoc2022::aoc09::one().unwrap_or_default(),
-        aoc2022::aoc09::two().unwrap_or_default()
-    );
-
-    print("2023-01", run(aoc2023::aoc01::one, aoc2023::aoc01::two));
-    print("2023-02", run(aoc2023::aoc02::one, aoc2023::aoc02::two));
-    print("2023-03", run(aoc2023::aoc03::one, aoc2023::aoc03::two));
-    print("2023-04", run(aoc2023::aoc04::one, aoc2023::aoc04::two));
-    print("2023-05", run(aoc2023::aoc05::one, aoc2023::aoc05::two));
-    print("2023-06", run(aoc2023::aoc06::one, aoc2023::aoc06::two));
-    print("2023-07", run(aoc2023::aoc07::one, aoc2023::aoc07::two));
-    print("2023-08", run(aoc2023::aoc08::one, aoc2023::aoc08::two));
-    print("2023-09", run(aoc2023::aoc09::one, aoc2023::aoc09::two));
-    print("2023-10", run(aoc2023::aoc10::one, aoc2023::aoc10::two));
-    print("2023-11", run(aoc2023::aoc11::one, aoc2023::aoc11::two));
-    print("2023-12", run(aoc2023::aoc12::one, aoc2023::aoc12::two));
-    print("2023-13", run(aoc2023::aoc13::one, aoc2023::aoc13::two));
+    match args.year.unwrap_or(date.year()) {
+        2021 => match args.day.unwrap_or(date.day()) {
+            1 => println!(
+                "2021-01: {} {}",
+                aoc2021::aoc01::one(),
+                aoc2021::aoc01::two()
+            ),
+            2 => println!(
+                "2021-02: {} {}",
+                aoc2021::aoc02::one(),
+                aoc2021::aoc02::two()
+            ),
+            3 => println!(
+                "2021-03: {} {}",
+                aoc2021::aoc03::one(),
+                aoc2021::aoc03::two()
+            ),
+            4 => println!(
+                "2021-04: {} {}",
+                aoc2021::aoc04::one(),
+                aoc2021::aoc04::two()
+            ),
+            d => println!("Cannot execute day {}", d),
+        },
+        2022 => match args.day.unwrap_or(date.day()) {
+            1 => println!(
+                "2022-01: {} {}",
+                aoc2022::aoc01::one(),
+                aoc2022::aoc01::two()
+            ),
+            2 => println!(
+                "2022-02: {} {}",
+                aoc2022::aoc02::one(),
+                aoc2022::aoc02::two()
+            ),
+            3 => println!(
+                "2022-03: {} {}",
+                aoc2022::aoc03::one(),
+                aoc2022::aoc03::two()
+            ),
+            4 => println!(
+                "2022-04: {} {}",
+                aoc2022::aoc04::one(),
+                aoc2022::aoc04::two()
+            ),
+            5 => println!(
+                "2022-05: {} {}",
+                aoc2022::aoc05::one(),
+                aoc2022::aoc05::two()
+            ),
+            6 => println!(
+                "2022-06: {} {}",
+                aoc2022::aoc06::one().unwrap_or_default(),
+                aoc2022::aoc06::two().unwrap_or_default()
+            ),
+            7 => println!(
+                "2022-07: {} {}",
+                aoc2022::aoc07::one().unwrap_or_default(),
+                aoc2022::aoc07::two().unwrap_or_default()
+            ),
+            8 => println!(
+                "2022-08: {} {}",
+                aoc2022::aoc08::one().unwrap_or_default(),
+                aoc2022::aoc08::two().unwrap_or_default()
+            ),
+            9 => println!(
+                "2022-09: {} {}",
+                aoc2022::aoc09::one().unwrap_or_default(),
+                aoc2022::aoc09::two().unwrap_or_default()
+            ),
+            d => println!("Cannot execute day {}", d),
+        },
+        2023 => match args.day.unwrap_or(date.day()) {
+            1 => print("2023-01", run(aoc2023::aoc01::one, aoc2023::aoc01::two)),
+            2 => print("2023-02", run(aoc2023::aoc02::one, aoc2023::aoc02::two)),
+            3 => print("2023-03", run(aoc2023::aoc03::one, aoc2023::aoc03::two)),
+            4 => print("2023-04", run(aoc2023::aoc04::one, aoc2023::aoc04::two)),
+            5 => print("2023-05", run(aoc2023::aoc05::one, aoc2023::aoc05::two)),
+            6 => print("2023-06", run(aoc2023::aoc06::one, aoc2023::aoc06::two)),
+            7 => print("2023-07", run(aoc2023::aoc07::one, aoc2023::aoc07::two)),
+            8 => print("2023-08", run(aoc2023::aoc08::one, aoc2023::aoc08::two)),
+            9 => print("2023-09", run(aoc2023::aoc09::one, aoc2023::aoc09::two)),
+            10 => print("2023-10", run(aoc2023::aoc10::one, aoc2023::aoc10::two)),
+            11 => print("2023-11", run(aoc2023::aoc11::one, aoc2023::aoc11::two)),
+            12 => print("2023-12", run(aoc2023::aoc12::one, aoc2023::aoc12::two)),
+            13 => print("2023-13", run(aoc2023::aoc13::one, aoc2023::aoc13::two)),
+            14 => print("2023-14", run(aoc2023::aoc14::one, aoc2023::aoc14::two)),
+            d => println!("Cannot execute day {}", d),
+        },
+        y => println!("Cannot execute year {}", y),
+    };
 }
