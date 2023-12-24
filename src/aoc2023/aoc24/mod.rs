8fe95c2c749b7cb5af89ac333671831adee4ac22
Jona Löffler <jona.loeffler@online.de>
2023-12-24 14:04:05 +0100

    Solve 2023-24

--- src/aoc2023/aoc24/mod.rs
+++ src/aoc2023/aoc24/mod.rs
@@ -1,5 +1,3 @@
-use std::collections::HashMap;
-
 use itertools::Itertools;
 
 static INPUT: &'static str = include_str!("./input");
@@ -44,24 +42,36 @@ fn parse() -> Vec<Hailstone> {
 }
 
 fn intersect(a: &Hailstone, b: &Hailstone) -> Option<(f64, f64)> {
-    let x1 = a.position[0];
-    let y1 = a.position[1];
-    let x2 = a.position[0] + a.velocity[0];
-    let y2 = a.position[1] + a.velocity[1];
+    // let x1 = a.position[0];
+    // let y1 = a.position[1];
+    // let x2 = a.position[0] + a.velocity[0];
+    // let y2 = a.position[1] + a.velocity[1];
 
-    let x3 = b.position[0];
-    let y3 = b.position[1];
-    let x4 = b.position[0] + b.velocity[0];
-    let y4 = b.position[1] + b.velocity[1];
+    // let x3 = b.position[0];
+    // let y3 = b.position[1];
+    // let x4 = b.position[0] + b.velocity[0];
+    // let y4 = b.position[1] + b.velocity[1];
 
-    let px = ((x1 * y2 - y1 * x2) * (x3 - x4) - (x1 - x2) * (x3 * y4 - y3 * x4))
-        / (((x1 - x2) * (y3 - y4)) - ((y1 - y2) * (x3 - x4)));
-    let py = ((x1 * y2 - y1 * x2) * (y3 - y4) - (y1 - y2) * (x3 * y4 - y3 * x4))
-        / (((x1 - x2) * (y3 - y4)) - ((y1 - y2) * (x3 - x4)));
+    // let px = ((x1 * y2 - y1 * x2) * (x3 - x4) - (x1 - x2) * (x3 * y4 - y3 * x4))
+    //     / (((x1 - x2) * (y3 - y4)) - ((y1 - y2) * (x3 - x4)));
+    // let py = ((x1 * y2 - y1 * x2) * (y3 - y4) - (y1 - y2) * (x3 * y4 - y3 * x4))
+    //     / (((x1 - x2) * (y3 - y4)) - ((y1 - y2) * (x3 - x4)));
+
+    let gradient_a = a.velocity[1] / a.velocity[0];
+    let gradient_b = b.velocity[1] / b.velocity[0];
+
+    let y_interval_a = a.position[1] - a.position[0] * gradient_a;
+    let y_interval_b = b.position[1] - b.position[0] * gradient_b;
+
+    // ga * x + ya = gb * x + yb
+    // ga * x - gb * x = yb - ya
+    // x = yb - ya / ga - gb
+
+    let px: f64 = (y_interval_b - y_interval_a) / (gradient_a - gradient_b);
+    let py: f64 = gradient_a * px + y_interval_a;
 
     if px.is_normal() && py.is_normal() {
-        // filter if is in the past
-        //
+        // Determine if collision is in the past
         // P = l + n * v;
         // P - l / v
         let nax = (px - a.position[0]) / a.velocity[0];
@@ -71,27 +81,19 @@ fn intersect(a: &Hailstone, b: &Hailstone) -> Option<(f64, f64)> {
         let nby = (py - b.position[1]) / b.velocity[1];
 
         if [nax, nay, nbx, nby].iter().any(|f| f < &0.0) {
-            println!("in the past");
             None
         } else {
-            println!("intersect at {:?}", (&px, &py));
             Some((px, py))
         }
     } else {
-        println!("parallel");
         None
     }
 }
 
 fn intersect_inside(a: &Hailstone, b: &Hailstone, low: f64, high: f64) -> bool {
     if let Some((a, b)) = intersect(a, b) {
-        let res = a > low && b > low && a < high && b < high;
-
-        println!("is inside: {:#?}", res);
-
-        res
+        a > low && b > low && a < high && b < high
     } else {
-        println!("no intersection in the future");
         false
     }
 }
